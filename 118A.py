6a30870db2e3b99993de2eaec6dbfb6aef59af5b
Yu Sawai <yusawai.null@gmail.com>
2012-02-06 22:23:23 +0900

    added 118A, 71A

---
+++ 118A.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+def solve(word):
+    vowels = ["A", "E", "I", "O", "U", "Y",
+              "a", "e", "i", "o", "u", "y"]
+    solved = ""
+    for i, alphabet in enumerate(word):
+        if alphabet in vowels:
+            pass
+        elif not alphabet in vowels:
+            solved += "." + alphabet.lower()
+    return solved
+
+def main():
+    word = raw_input()
+    solved = solve(word)
+    print solved
+
+
+
+if __name__=="__main__":
+    main()
