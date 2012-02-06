4bb36d636dbd26526bd8d87be19f5300b6aad214
Yu Sawai <yusawai.null@gmail.com>
2012-02-06 19:18:47 +0900

    add some line to be looked better

--- 1A.hs
+++ 1A.hs
@@ -3,7 +3,9 @@ fillSquare :: Int -> Int-> Int -> Integer
 fillSquare n m a = widthnum * heightnum
     where widthnum = ceiling((fromIntegral (n :: Int))/(fromIntegral (a :: Int )))
           heightnum = ceiling((fromIntegral (m :: Int))/(fromIntegral (a :: Int)))
--- | main with IO
+
+
+-- | main 
 main = do
     sn <- getLine
     let sl = words sn
