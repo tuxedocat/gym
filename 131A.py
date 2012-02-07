f961fc0dd32fd16aeb68b90ac0ffb9aaced45ba1
Yu Sawai <yusawai.null@gmail.com>
2012-02-07 11:52:20 +0900

    deleted swap files

---
+++ 131A.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+def solve(word):
+    solved = ""
+    if word.isupper():
+        return word.lower()
+    elif len(word)==1 and word.islower():
+        return word.upper()
+    elif word[0].islower() and word[1:].isupper():
+        solved = word[0].upper() + word[1:].lower()
+        return solved
+    else:
+        return word
+
+def main():
+    word = raw_input()
+    solved = solve(word)
+    print solved
+
+
+
+if __name__=="__main__":
+    main()
