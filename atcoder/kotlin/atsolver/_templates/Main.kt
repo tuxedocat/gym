19fb5903e527750b6f1deb2b6ddbd23746251d30
tuxedocat <tuxedocat@users.noreply.github.com>
2019-07-16 09:04:56 +0900

    Update kotlin template

--- atcoder/kotlin/atsolver/_templates/Main.kt
+++ atcoder/kotlin/atsolver/_templates/Main.kt
@@ -1,7 +1,15 @@
 package {{pkgname}}
 
 fun main(args: Array<String>) {
-    val x = readLine()!!.split(' ').map {String::toInt()}
+    val sc = java.util.Scanner(System.`in`)
+    val n = sc.nextInt()
+    val m = sc.nextInt()
+    val ss: List<Set<Int>> = (1..m).map {
+        val k = sc.nextInt()
+        sc.asSequence().take(k).map(String::toInt).map { it - 1 }.toSet()
+    }
+    val ps = sc.asSequence().take(m).map(String::toInt).toList()    val x = readLine()!!.split(' ').map {String::toInt()}
+
     val ans = solve(42)
     println(ans)
 }
