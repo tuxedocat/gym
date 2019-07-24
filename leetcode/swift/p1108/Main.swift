8dd43b4d0b9a9fbd9bef49c7f48d23643a1a55fc
tuxedocat <tuxedocat@users.noreply.github.com>
2019-07-25 00:25:09 +0900

    Write swift in vscode

---
+++ leetcode/swift/p1108/Main.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func defangIPaddr(_ address: String) -> String {
+    let ans = address.replacingOccurrences(of: ".", with: "[.]")
+    return ans
+}
+
+func main() {
+    let addr1 = "255.100.50.0"
+    print("\(defangIPaddr(addr1))")
+}
+
+main()
